f089301bc75f66c1aaa60c885d28a9af79942d55
Mohamed Hussein <m.hussien.android@gmail.com>
2022-01-11 10:08:25 +0100

    Migrate from livedata to flow

--- app/src/main/java/com/application/flickerimagesearch/ui/main/viewmodel/MainViewModel.kt
+++ app/src/main/java/com/application/flickerimagesearch/ui/main/viewmodel/MainViewModel.kt
@@ -1,14 +1,15 @@
 package com.application.flickerimagesearch.ui.main.viewmodel
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.application.flickerimagesearch.data.model.PhotoResponse
 import com.application.flickerimagesearch.data.repository.PhotosRepository
 import com.application.flickerimagesearch.utils.NetworkHelper
 import com.application.flickerimagesearch.utils.Resource
+import com.application.flickerimagesearch.utils.Status
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.launch
 import javax.inject.Inject
@@ -19,24 +20,21 @@ class MainViewModel @Inject constructor(
     private val networkHelper: NetworkHelper
 ) : ViewModel() {
 
-    private val _photos = MutableLiveData<Resource<List<PhotoResponse>>>()
-    val photos: LiveData<Resource<List<PhotoResponse>>>
-        get() = _photos
+    private val _uiState = MutableStateFlow(Resource<List<PhotoResponse>>(Status.SUCCESS, emptyList(), null))
+    val uiState: StateFlow<Resource<List<PhotoResponse>>> = _uiState
 
     fun fetchImages(searchText: String) {
         viewModelScope.launch {
-            _photos.postValue(Resource.loading(null))
+            _uiState.value = Resource.loading(null)
             if (networkHelper.isNetworkConnected()) {
                 mainRepository.fetchImages(searchText).let { responseFlow ->
-                    responseFlow.collect() { response ->
+                    responseFlow.collect { response ->
                         if (response.isSuccessful) {
-                            _photos.postValue(Resource.success(response.body()?.photos?.photo))
-                        } else _photos.postValue(Resource.error(response.errorBody().toString(), null))
+                            _uiState.value = Resource.success(response.body()?.photos?.photo)
+                        } else _uiState.value = Resource.error(response.errorBody().toString(), null)
                     }
                 }
-            } else _photos.postValue(Resource.error("No internet connection", null))
+            } else _uiState.value = Resource.error("No internet connection", null)
         }
     }
 }
-
-
